9cfb0289db3bcd88fa65dca629b9821248680582
D. M <David@Julias-MacBook-Pro-2.local>
2020-01-20 17:19:29 +0100

    Completes the header file of the obj-c demo

--- FSLibIOsObjcDemo/FSLibIOsObjcDemo/ViewController.h
+++ FSLibIOsObjcDemo/FSLibIOsObjcDemo/ViewController.h
@@ -11,11 +11,70 @@
 
 @interface ViewController : UIViewController <FSNavigationControllerDelegate> {
     // Interface to the belt
-    //FSNavigationController *beltController;
+    FSNavigationController *beltController;
 }
-@property(nonatomic, retain) FSNavigationController *beltController;
-//- (void)updateUILabels;
 
+// Properties
+@property(nonatomic, retain) FSNavigationController *beltController;
+@property(nonatomic, assign) FSBeltVibrationSignal selectedSignalType;
+
+// UI components
+@property(weak, nonatomic) IBOutlet UIButton *connectButton;
+@property(weak, nonatomic) IBOutlet UIButton *disconnectButton;
+@property(weak, nonatomic) IBOutlet UILabel *connectionStateLabel;
+@property(weak, nonatomic) IBOutlet UILabel *defaultIntensityLabel;
+@property(weak, nonatomic) IBOutlet UISlider *defaultIntensitySlider;
+@property(weak, nonatomic) IBOutlet UILabel *beltHeadingLabel;
+@property(weak, nonatomic) IBOutlet UILabel *orientationAccurateLabel;
+@property(weak, nonatomic) IBOutlet UIButton *changeAccuracySignalButton;
+@property(weak, nonatomic) IBOutlet UILabel *powerStatusLabel;
+@property(weak, nonatomic) IBOutlet UILabel *batteryLevelLabel;
+@property(weak, nonatomic) IBOutlet UIButton *startBatterySignalButton;
+@property(weak, nonatomic) IBOutlet UILabel *navigationDirectionLabel;
+@property(weak, nonatomic) IBOutlet UISlider *navigationDirectionSlider;
+@property(weak, nonatomic) IBOutlet UISwitch *magneticBearingSwitch;
+@property(weak, nonatomic) IBOutlet UIButton *signalTypeButton;
+@property(weak, nonatomic) IBOutlet UIButton *startNavigationButton;
+@property(weak, nonatomic) IBOutlet UIButton *pauseNavigationButton;
+@property(weak, nonatomic) IBOutlet UIButton *stopNavigationButton;
+@property(weak, nonatomic) IBOutlet UILabel *navigationStateLabel;
+@property(weak, nonatomic) IBOutlet UILabel *notificationDirectionLabel;
+@property(weak, nonatomic) IBOutlet UISlider *notificationDirectionSlider;
+@property(weak, nonatomic) IBOutlet UIButton *startBearingNotificationButton;
+@property(weak, nonatomic) IBOutlet UIButton *startDirectionNotificationButton;
+@property(weak, nonatomic) IBOutlet UIButton *startWarningButton;
+@property(weak, nonatomic) IBOutlet UIButton *startCriticalWarningButton;
+
+// Private methods
+- (void)updateUI;
+- (void)updateConnectionPanel;
+- (void)updateDefaultIntensityPanel;
+- (void)updateBatteryPanel;
+- (void)updateOrientationPanel;
+- (void)updateNavigationSignalTypePanel;
+- (void)updateNavigationStatePanel;
+- (void)setSignalType:(FSBeltVibrationSignal)selected;
+- (void)showToast:(NSString*)message;
+
+// UI event handlers
+- (IBAction)onConnectButtonTap:(id)sender;
+- (IBAction)onDisconnectButtonTap:(id)sender;
+- (IBAction)onDefaultIntensitySliderValueChanged:(id)sender;
+- (IBAction)onDefaultIntensitySliderReleased:(id)sender;
+- (IBAction)onDefaultIntensitySliderReleasedOutside:(id)sender;
+- (IBAction)onChangeAccuracySignalButtonTap:(id)sender;
+- (IBAction)onStartBatterySignalButtonTap:(id)sender;
+- (IBAction)onNavigationDirectionSliderValueChanged:(id)sender;
+- (IBAction)onMagneticBearingSwitchValueChanged:(id)sender;
+- (IBAction)onSignalTypeButtonTap:(id)sender;
+- (IBAction)onStartNavigationButtonTap:(id)sender;
+- (IBAction)onPauseNavigationButtonTap:(id)sender;
+- (IBAction)onStopNavigationButtonTap:(id)sender;
+- (IBAction)onNotificationDirectionSliderValueChanged:(id)sender;
+- (IBAction)onStartBearingNotificationButtonTap:(id)sender;
+- (IBAction)onStartDirectionNotificationButtonTap:(id)sender;
+- (IBAction)onStartWarningButtonTap:(id)sender;
+- (IBAction)onStartCriticalWarningButtonTap:(id)sender;
 
 @end
 
--- FSLibIOsObjcDemo/FSLibIOsObjcDemo/ViewController.m
+++ FSLibIOsObjcDemo/FSLibIOsObjcDemo/ViewController.m
@@ -14,10 +14,209 @@
 
 @implementation ViewController
 
+@synthesize beltController;
+@synthesize selectedSignalType;
+
+// UI components
+@synthesize connectButton;
+@synthesize disconnectButton;
+@synthesize connectionStateLabel;
+@synthesize defaultIntensityLabel;
+@synthesize defaultIntensitySlider;
+@synthesize beltHeadingLabel;
+@synthesize orientationAccurateLabel;
+@synthesize changeAccuracySignalButton;
+@synthesize powerStatusLabel;
+@synthesize batteryLevelLabel;
+@synthesize startBatterySignalButton;
+@synthesize navigationDirectionLabel;
+@synthesize navigationDirectionSlider;
+@synthesize magneticBearingSwitch;
+@synthesize signalTypeButton;
+@synthesize startNavigationButton;
+@synthesize pauseNavigationButton;
+@synthesize stopNavigationButton;
+@synthesize navigationStateLabel;
+@synthesize notificationDirectionLabel;
+@synthesize notificationDirectionSlider;
+@synthesize startBearingNotificationButton;
+@synthesize startDirectionNotificationButton;
+@synthesize startWarningButton;
+@synthesize startCriticalWarningButton;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // Belt controller
+    beltController = [[FSNavigationController alloc] init];
+    [beltController setDelegate:self];
+    // Selected signal type
+    selectedSignalType = FSBeltVibrationSignalNoVibration;
+    // Update UI
+    [self updateUI];
 }
 
+//MARK: Private methods
+
+- (void)updateUI {
+    //MARK: TODO
+}
+
+- (void)updateConnectionPanel {
+    //MARK: TODO
+}
+
+- (void)updateDefaultIntensityPanel {
+    //MARK: TODO
+}
+
+- (void)updateBatteryPanel {
+    //MARK: TODO
+}
+
+- (void)updateOrientationPanel {
+    //MARK: TODO
+}
+
+- (void)updateNavigationSignalTypePanel {
+    //MARK: TODO
+}
+
+- (void)updateNavigationStatePanel {
+    //MARK: TODO
+}
+
+- (void)setSignalType:(FSBeltVibrationSignal)selected {
+    //MARK: TODO
+}
+
+- (void)showToast:(NSString*)message {
+    //MARK: TODO
+}
+
+
+//MARK: UI Event handlers
+
+- (IBAction)onConnectButtonTap:(id)sender{
+    [beltController searchAndConnectBelt];
+}
+
+- (IBAction)onDisconnectButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onDefaultIntensitySliderValueChanged:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onDefaultIntensitySliderReleased:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onDefaultIntensitySliderReleasedOutside:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onChangeAccuracySignalButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onStartBatterySignalButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onNavigationDirectionSliderValueChanged:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onMagneticBearingSwitchValueChanged:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onSignalTypeButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onStartNavigationButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onPauseNavigationButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onStopNavigationButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onNotificationDirectionSliderValueChanged:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onStartBearingNotificationButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onStartDirectionNotificationButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onStartWarningButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+- (IBAction)onStartCriticalWarningButtonTap:(id)sender{
+    //MARK: TODO
+}
+
+
+//MARK: Delegate methods implementation
+
+- (void)onBeltBatteryLevelUpdatedWithBatteryLevel:(NSInteger)batteryLevel status:(enum FSPowerStatus)status {
+    //TODO: TBI
+}
+
+- (void)onBeltConnectionFailed {
+    //TODO: TBI
+}
+
+- (void)onBeltConnectionLost {
+    //TODO: TBI
+}
+
+- (void)onBeltConnectionStateChangedWithState:(enum FSBeltConnectionState)state {
+    //TODO: TBI
+}
+
+- (void)onBeltDefaultVibrationIntensityChangedWithIntensity:(NSInteger)intensity {
+    //TODO: TBI
+}
+
+- (void)onBeltHomeButtonPressedWithNavigating:(BOOL)navigating {
+    //TODO: TBI
+}
+
+- (void)onBeltOrientationUpdatedWithBeltHeading:(NSInteger)beltHeading accurate:(BOOL)accurate {
+    //TODO: TBI
+}
+
+- (void)onBluetoothNotAvailable {
+    //TODO: TBI
+}
+
+- (void)onBluetoothPoweredOff {
+    //TODO: TBI
+}
+
+- (void)onCompassAccuracySignalStateUpdatedWithEnabled:(BOOL)enabled {
+    //TODO: TBI
+}
+
+- (void)onNavigationStateChangeWithState:(enum FSNavigationState)state {
+    //TODO: TBI
+}
+
+- (void)onNoBeltFound {
+    //TODO: TBI
+}
 
 @end
